16e035929c0880b5a86b8560dd31aa5ff659f100
Vladimir <lenta-b@yandex.ru>
2017-01-22 22:22:03 +0300

    v0.4.0

--- js/main.js
+++ js/main.js
@@ -5,39 +5,46 @@ if (xhr.status != 200) {
 	console.log('error');
 } else {
 	var data = JSON.parse(xhr.responseText);
-	
-	
+
+
 	var source = entrytemplate.innerHTML;
-					var templateFn = Handlebars.compile(source);
-					var context = templateFn({
-						list: data
-					});
+	var templateFn = Handlebars.compile(source);
+	var context = templateFn({
+		list: data
+	});
 	var element = document.getElementById('results');
-	element.innerHTML = context;	
+	element.innerHTML = context;
 }
 
 
 var check = document.getElementsByClassName("checkbox");
+var items = check.length;
+var findAncestor = (target, needClass) => {
+	while ((target = target.parentElement) && !target.classList.contains(needClass));
+
+	target.classList.add('greenBackgound');
+
+
+	setTimeout(() => {
+		target.parentElement.style.display = "none";
+	}, 1000);
+	items -= 1;
+	if (!items) {
+		document.getElementById("complete_block").style.display = "block";
+		document.getElementById("courses_wrap").style.display = "none";
+	}
+}
+var needClass = "course_item_text";
 
 for (var i = 0; i < check.length; i++) {
-	check[i].addEventListener("change", function(e) {
-	var target = e.target;
-	var needClass = "course_item_text";	
-		
-	var findAncestor = (target, needClass) => {
-		 while ((target = target.parentElement) && !target.classList.contains(needClass));
-		
-		target.classList.add('greenBackgound');
-		
-		
-		setTimeout(()=>{target.parentElement.style.display="none"; console.log (1111);},1000);
-		
-		console.log (target);
-	} 	
-	
-	findAncestor (target,needClass);
-	
-});
-}
+
+	check[i].addEventListener("change", function (e) {
+		var target = e.target;
+
+
+		findAncestor(target, needClass);
+	});
+};
+
 
 
